7be853e632660c039adbe322da8e89017a225c47
vadshe <vadshe@8f761e62-4f20-5a59-19ca-588f4a44a45b>
2010-05-13 13:28:55 +0000

    GTK changes.

--- gtk/walet_gtk.c
+++ gtk/walet_gtk.c
@@ -1,22 +1,95 @@
-
 #include <walet_gtk.h>
 
-void  reset_default_status (WaletGTK *wlgtk)
+
+void  print_status(WaletGTK *wlgtk, const gchar *mesage)
 {
-	gchar           *file;
-	gchar           *status;
+	//gchar	*file;
+	//gchar	*status;
 
-	if (wlgtk->filename == NULL)
-	{
-		file = g_strdup ("(UNTITLED)");
-	}
-	else file = g_path_get_basename (wlgtk->filename);
+	//if (wlgtk->filename == NULL)
+	//{
+	//	file = g_strdup ("NULL");
+	//}
+	//else file = g_path_get_basename (wlgtk->filename);
 
-	status = g_strdup_printf ("File: %s", file);
+	//status = g_strdup_printf ("File: %s", file);
 	gtk_statusbar_pop (GTK_STATUSBAR (wlgtk->statusbar), wlgtk->statusbar_context_id);
-	gtk_statusbar_push(GTK_STATUSBAR (wlgtk->statusbar), wlgtk->statusbar_context_id, status);
-	g_free (status);
-	g_free (file);
+	gtk_statusbar_push(GTK_STATUSBAR (wlgtk->statusbar), wlgtk->statusbar_context_id, mesage);
+	//g_free (status);
+	//g_free (file);
+}
+
+void error_message(const gchar *message)
+{
+        GtkWidget               *dialog;
+
+        /* log to terminal window */
+        g_warning (message);
+
+        /* create an error message dialog and display modally to the user */
+        dialog = gtk_message_dialog_new(NULL,
+										GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
+										GTK_MESSAGE_ERROR,
+										GTK_BUTTONS_OK,
+										message);
+
+        gtk_window_set_title (GTK_WINDOW (dialog), "Error!");
+        gtk_dialog_run (GTK_DIALOG (dialog));
+        gtk_widget_destroy (dialog);
+}
+
+void on_quit_activate(GtkObject *object, WaletGTK *wlgtk)
+{
+    gtk_main_quit ();
+}
+
+
+void on_open_activate(GtkObject *object, WaletGTK *wlgtk)
+{
+	GtkWidget	*chooser;
+	GError  	*err=NULL;
+	guint		i, size;
+	gchar		**line;
+
+	chooser = gtk_file_chooser_dialog_new ("Open File...",
+											GTK_WINDOW (wlgtk->window),
+											GTK_FILE_CHOOSER_ACTION_OPEN,
+											GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+											GTK_STOCK_OPEN, GTK_RESPONSE_OK,
+											NULL);
+
+	if (gtk_dialog_run(GTK_DIALOG (chooser)) == GTK_RESPONSE_OK) {
+		wlgtk->filename = gtk_file_chooser_get_filename (GTK_FILE_CHOOSER (chooser));
+		print_status(wlgtk, wlgtk->filename);
+	} else {
+		g_error("Could'n open file\n");
+	}
+	gtk_widget_destroy (chooser);
+
+    // Get the file contents
+    if (g_file_get_contents (wlgtk->filename, &wlgtk->buff, &size, &err) == FALSE) {
+    	error_message (err->message);
+    	g_error_free (err);
+    	//g_free (wlgtk->filename);
+    }
+    //g_message("File size = %d\n", size);
+    line[0] = g_strdup(wlgtk->buff);
+    size = g_strv_length(line);
+    g_message("srt = %s  size = %d\n",line[0], size);
+    size = sscanf(wlgtk->buff, "%s%s%s%s", line[0], line[1], line[2], line[3]);
+    if (strcmp(line[0], "P5") != 0) {
+		  g_warning("It's not PGM file");
+		  g_free(wlgtk->buff);
+		  //return FALSE;
+    }
+    wlgtk->sd->width  	= atoi(line[1]);
+    wlgtk->sd->height 	= atoi(line[2]);
+    wlgtk->sd->bits 	= atoi(line[3]);
+    size = 0;
+    for(i=0; i<4; i++) size += strlen(line[i]);
+
+    print_status(wlgtk, g_strdup_printf ("width = %d  height = %d bpp = %d offset = %d", wlgtk->sd->width, wlgtk->sd->height, wlgtk->sd->bits, size));
+
 }
 
 gboolean  init_wlgtk(WaletGTK *wlgtk)
@@ -26,7 +99,7 @@ gboolean  init_wlgtk(WaletGTK *wlgtk)
 	guint                   id;
 	PangoFontDescription    *font_desc;
 
-	/* use GtkBuilder to build our interface from the XML file */
+	// use GtkBuilder to build our interface from the XML file
 	builder = gtk_builder_new();
 	if (gtk_builder_add_from_file(builder, "walet_gtk.xml", &err) == 0){
 		error_message(err->message);
@@ -34,106 +107,56 @@ gboolean  init_wlgtk(WaletGTK *wlgtk)
 		return FALSE;
 	}
 
-	/* get the widgets which will be referenced in callbacks */
-	wlgtk->window = GTK_WIDGET (gtk_builder_get_object (builder, "window1"));
-	wlgtk->statusbar = GTK_WIDGET (gtk_builder_get_object (builder, "statusbar1"));
-	//editor->text_view = GTK_WIDGET (gtk_builder_get_object (builder, "text_view"));
+	// get the widgets which will be referenced in callbacks
+	wlgtk->window = GTK_WIDGET (gtk_builder_get_object (builder, "window"));
+	wlgtk->statusbar = GTK_WIDGET (gtk_builder_get_object (builder, "statusbar"));
+	wlgtk->menu_new =  GTK_WIDGET (gtk_builder_get_object (builder, "menu_new"));
+	wlgtk->menu_open =  GTK_WIDGET (gtk_builder_get_object (builder, "menu_open"));
+	wlgtk->menu_save =  GTK_WIDGET (gtk_builder_get_object (builder, "menu_save"));
+	wlgtk->menu_save_as =  GTK_WIDGET (gtk_builder_get_object (builder, "menu_save_as"));
+	wlgtk->menu_quit =  GTK_WIDGET (gtk_builder_get_object (builder, "menu_quit"));
 
-	/* connect signals, passing our TutorialTextEditor struct as user data */
-	gtk_builder_connect_signals (builder, wlgtk);
+	//connect signals, don't work now
+	//gtk_builder_connect_signals(builder, wlgtk);
 
-	/* free memory used by GtkBuilder object */
+    g_signal_connect(wlgtk->window, "destroy", G_CALLBACK (on_quit_activate), wlgtk);
+    g_signal_connect(wlgtk->menu_open, "activate", G_CALLBACK (on_open_activate), wlgtk);
+    g_signal_connect(wlgtk->menu_quit, "activate", G_CALLBACK (on_quit_activate), wlgtk);
+
+	// free memory used by GtkBuilder object
 	g_object_unref (G_OBJECT (builder));
 
-	/* set the text view font */
-	//font_desc = pango_font_description_from_string ("monospace 10");
-	//gtk_widget_modify_font (editor->text_view, font_desc);
-	//pango_font_description_free (font_desc);
-
-	/* set the default icon to the GTK "edit" icon */
+	// set the default icon to the GTK "edit" icon
 	gtk_window_set_default_icon_name (GTK_STOCK_EDIT);
 
-	/* setup and initialize our statusbar */
-	id = gtk_statusbar_get_context_id (GTK_STATUSBAR (wlgtk->statusbar), "Tutorial GTK+ Text Editor");
-	wlgtk->statusbar_context_id = id;
-	//reset_default_status (wlgtk);
-
-	/* set filename to NULL since we don't have an open/saved file yet */
 	wlgtk->filename = NULL;
+	// setup and initialize our statusbar
+	wlgtk->statusbar_context_id = gtk_statusbar_get_context_id (GTK_STATUSBAR (wlgtk->statusbar), "Open file");
+	print_status(wlgtk, "No file open");
 
 	return TRUE;
 }
 
-void error_message (const gchar *message)
-{
-        GtkWidget               *dialog;
-
-        /* log to terminal window */
-        g_warning (message);
-
-        /* create an error message dialog and display modally to the user */
-        dialog = gtk_message_dialog_new (NULL,
-                                         GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
-                                         GTK_MESSAGE_ERROR,
-                                         GTK_BUTTONS_OK,
-                                         message);
-
-        gtk_window_set_title (GTK_WINDOW (dialog), "Error!");
-        gtk_dialog_run (GTK_DIALOG (dialog));
-        gtk_widget_destroy (dialog);
-}
-
-gchar * get_open_filename (WaletGTK *wlgtk)
-{
-	GtkWidget               *chooser;
-	gchar                   *filename=NULL;
-
-	chooser = gtk_file_chooser_dialog_new ("Open File...",
-											GTK_WINDOW (wlgtk->window),
-											GTK_FILE_CHOOSER_ACTION_OPEN,
-											GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
-											GTK_STOCK_OPEN, GTK_RESPONSE_OK,
-											NULL);
-
-	if (gtk_dialog_run (GTK_DIALOG (chooser)) == GTK_RESPONSE_OK){
-		filename = gtk_file_chooser_get_filename (GTK_FILE_CHOOSER (chooser));
-	}
-	gtk_widget_destroy (chooser);
-	return filename;
-}
-
-void on_Open1_activate(GtkMenuItem *menuitem, WaletGTK *wlgtk)
-{
-	gchar	*filename;
-
-	//if (check_for_save(wlgtk) == TRUE){
-	//	on_save_menu_item_activate (NULL, editor);
-	//}
-	g_warning ("on_Open1_activate\n");
-	filename = get_open_filename (wlgtk);
-	//if (filename != NULL) load_file (wlgtk, filename);
-}
 
 int main (int argc, char *argv[])
 {
-	WaletGTK      *wlgtk;
-
-	/* allocate the memory needed by our TutorialTextEditor struct */
-	wlgtk = g_slice_new (WaletGTK);
-
-	/* initialize GTK+ libraries */
+	WaletGTK	*wlgtk;
+	StreamData	*sd;
+	GOP			*gop;
+	// Allocate the memory needed by our WaletGTK struct
+	wlgtk 	= g_slice_new(WaletGTK);
+	sd 		= g_slice_new(StreamData);
+	gop		= g_slice_new(GOP);
+	wlgtk->sd 	= sd;
+	wlgtk->gop	= gop;
+	// Initialize GTK+ libraries
 	gtk_init (&argc, &argv);
-
-	if (init_wlgtk(wlgtk) == FALSE) return 1; /* error loading UI */
-
-	/* show the window */
+	if (init_wlgtk(wlgtk) == FALSE) return 1; // error loading UI
+	// Show the window
 	gtk_widget_show (wlgtk->window);
-
-	/* enter GTK+ main loop */
+	// Enter GTK+ main loop
 	gtk_main ();
-
-	/* free memory we allocated for TutorialTextEditor struct */
+	// Free memory we allocated for TutorialTextEditor struct
 	g_slice_free (WaletGTK, wlgtk);
-
 	return 0;
 }
--- gtk/walet_gtk.h
+++ gtk/walet_gtk.h
@@ -1,10 +1,27 @@
-#include <gtk/gtk.h>
-
+#include 	<gtk/gtk.h>
+#include	<walet.h>
 typedef struct
 {
 	GtkWidget               *window;
 	GtkWidget               *statusbar;
-	GtkWidget               *text_view;
+	//Menu widgets
+	GtkWidget				*menu_new;
+	GtkWidget				*menu_open;
+	GtkWidget				*menu_save;
+	GtkWidget				*menu_save_as;
+	GtkWidget				*menu_quit;
+
 	gchar                   *filename;
 	guint					statusbar_context_id;
+	gchar					*buff; //Buffer for image loading.
+
+	StreamData				*sd;
+	GOP						*gop;
 } WaletGTK;
+
+// File menu callbacks prototypes
+void on_new_activate 		(GtkObject *object, WaletGTK *wlgtk);
+void on_open_activate 		(GtkObject *object, WaletGTK *wlgtk);
+void on_save_activate 		(GtkObject *object, WaletGTK *wlgtk);
+void on_save_as_activate 	(GtkObject *object, WaletGTK *wlgtk);
+void on_quit_activate 		(GtkObject *object, WaletGTK *wlgtk);
