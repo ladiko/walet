cc0e1b97b1a61a1896f7bf27b1b56aafbca06638
vadshe <vadshe@8f761e62-4f20-5a59-19ca-588f4a44a45b>
2011-02-17 02:31:01 +0000

    the difference picture

--- lib/segmentation.h
+++ lib/segmentation.h
@@ -25,6 +25,8 @@ void seg_draw_lines(Pixel *pix, uint32 npix, imgtype *img, uint32 w, uint32 h);
 void seg_compare(Pixel *pix, Pixel *pix1, imgtype *grad1, imgtype *grad2, imgtype *img1, imgtype *img2, uint32 w, uint32 h);
 void seg_draw_vec(Pixel *pix, uint32 npix, imgtype *img, uint32 w, uint32 h);
 void seg_draw_pix(Pixel *pix, uint32 npix, imgtype *img, uint32 w, uint32 h);
+void seg_intersect_pix(imgtype *img1, imgtype *img2, uint32 w, uint32 h);
+void seg_mvector_copy(Pixel *pix, imgtype *grad1, imgtype *img1, imgtype *img2, uint32 w, uint32 h);
 
 #ifdef __cplusplus
 }
