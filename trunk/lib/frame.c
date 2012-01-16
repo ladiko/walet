a61999bbeb15852757a684a379f4c05931b6854d
vadshe <vadshe@8f761e62-4f20-5a59-19ca-588f4a44a45b>
2012-01-16 13:25:56 +0000

    Some changes.

--- trunk/lib/frame.c
+++ trunk/lib/frame.c
@@ -831,13 +831,9 @@ uint32 frame_segmetation(GOP *g, uint32 fn, WaletConfig *wc)
 		//seg_remove_vertex(f->dc[i].pic, f->rg[i].pic, f->dg[i].w, f->dg[i].h);
 
 		seg_find_intersect3(f->dg[i].pic, f->rg[i].pic, f->di[i].pic, f->dg[i].w, f->dg[i].h);
-
 		seg_fill_reg(f->rg[i].pic, (uint32*)g->buf, f->dg[i].w, f->dg[i].h);
-
 		seg_remove_contour(f->rg[i].pic, f->dg[i].w, f->dg[i].h);
-
 		vxc = seg_vertex2(f->rg[i].pic, f->di[i].pic, f->vx, f->vp, f->dg[i].w, f->dg[i].h);
-
 		seg_remove_virtex(f->vp, vxc, f->dg[i].w, f->dg[i].h);
 		seg_vertex_draw(f->R1[i].pic, f->G1[i].pic, f->B1[i].pic, f->vp, vxc, f->R1[i].w);
 
