43741e7604ddad7db00f7439c4b9d49b366af1a1
vadshe <vadshe@8f761e62-4f20-5a59-19ca-588f4a44a45b>
2012-03-19 12:58:11 +0000

    seg_remove_loops func remove any single line connection regions.

--- lib/frame.c
+++ lib/frame.c
@@ -156,7 +156,7 @@ void frame_init(GOP *g, uint32 fn, WaletConfig *wc)
 	//f->ln = (Line *)calloc((w>>1)*(h>>1), sizeof(Line));
 	//f->lp = (Line **)calloc(w*h, sizeof(Line*));
 	f->vx = (Vertex *)calloc(w*h, sizeof(Vertex));
-	f->vp = (Vertex **)calloc(w*h>>1, sizeof(Vertex*));
+	f->vp = (Vertex **)calloc(w*h, sizeof(Vertex*));
 
 	//Old init
 	f->size = w*h;
@@ -835,7 +835,7 @@ uint32 frame_segmetation(GOP *g, uint32 fn, WaletConfig *wc)
 		seg_find_intersect4(f->dg[0].pic, f->dc[0].pic, f->di[0].pic, f->y[1].w, f->y[1].h);
 		vxc = seg_vertex3(f->dc[0].pic, f->di[0].pic, f->vx, f->vp, g->buf, f->y[1].w, f->y[1].h);
 		seg_remove_virtex(f->vp, vxc, f->y[1].w, f->y[1].h);
-		seg_remove_loops(f->y1[1].pic, f->vp, &f->vp[wc->w*wc->h>>2], vxc, f->y[1].w, f->y[1].h);
+		seg_remove_loops(f->y1[1].pic, f->vp, &f->vp[wc->w*wc->h>>2], &f->vp[wc->w*wc->h>>1], vxc, f->y[1].w, f->y[1].h);
 		memset(f->di[0].pic, 0, f->dg[0].w*f->dg[0].h);
 		//seg_vertex_draw3(f->di[0].pic, f->vp, (uint32*)g->buf, vxc, f->y[1].w, f->y[1].h, f->y[1].w, f->y[1].h);
 		//seg_vertex_draw3(f->y1[0].pic, f->vp, (uint32*)g->buf, vxc, f->y[0].w, f->y[0].h, f->y[1].w, f->y[1].h);
