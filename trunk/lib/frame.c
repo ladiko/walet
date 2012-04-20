dcec051583daee856c07cef42030ad26da3c86c4
vadshe <vadshe@8f761e62-4f20-5a59-19ca-588f4a44a45b>
2012-04-20 13:33:28 +0000

    Some changes from China.

--- trunk/lib/frame.c
+++ trunk/lib/frame.c
@@ -868,8 +868,8 @@ uint32 frame_segmetation(GOP *g, uint32 fn, WaletConfig *wc)
 		//seg_vertex_draw3(f->y1[0].pic, f->vp, (uint32*)g->buf, vxc, f->y[0].w, f->y[0].h, f->y[1].w, f->y[1].h);
 		//seg_vertex_draw3(f->y1[1].pic, f->vp, (uint32*)g->buf, vxc, f->y[1].w, f->y[1].h, f->y[1].w, f->y[1].h);
 
-		seg_remove_inline(f->vp, vxc, f->y[1].w, f->y[1].h);
-		seg_remove_virtex(f->vp, vxc, f->y[1].w, f->y[1].h);
+		//seg_remove_inline(f->vp, vxc, f->y[1].w, f->y[1].h);
+		//seg_remove_virtex(f->vp, vxc, f->y[1].w, f->y[1].h);
 
 		rgc = seg_remove_loops(f->y1[1].pic, f->vp, &f->vpt[wc->w*wc->h>>2], f->vpt, f->ln, f->dm[0].pic, vxc, f->y[1].w, f->y[1].h);
 		seg_vertex_draw3(f->y1[1].pic, f->vp,  vxc, f->y[1].w, f->y[1].h, f->y[1].w, f->y[1].h);
@@ -878,7 +878,7 @@ uint32 frame_segmetation(GOP *g, uint32 fn, WaletConfig *wc)
 		//for(i=0; i <  f->y[1].w*f->y[1].h; i++) f->y1[1].pic[i] = 0;
 
 		//seg_vertex_draw3(f->di[0].pic, f->vp, (uint32*)g->buf, vxc, f->y[1].w, f->y[1].h, f->y[1].w, f->y[1].h);
-
+/*
 		seg_get_or_fill_color(f->y[1].pic, f->y1[1].pic, f->dm[1].pic, (uint32*)g->buf, f->vpt, f->dm[0].pic,
 				rgc, f->y[1].w, f->y[1].h, f->y[1].w, f->y[1].h, 1);
 
@@ -887,7 +887,7 @@ uint32 frame_segmetation(GOP *g, uint32 fn, WaletConfig *wc)
 
 		seg_get_or_fill_color(NULL, f->y1[0].pic, f->dm[1].pic, (uint32*)g->buf, f->vpt, f->dm[0].pic,
 				rgc, f->y[0].w, f->y[0].h, f->y[1].w, f->y[1].h, 0);
-
+*/
 
 		//seg_draw_line_one(f->y1[0].pic, f->y[0].w, f->y[0].h);
 
