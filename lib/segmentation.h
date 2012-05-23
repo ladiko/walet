d282e7976c476fad20300db9670b65d930a2b503
vadshe <vadshe@8f761e62-4f20-5a59-19ca-588f4a44a45b>
2012-05-23 13:56:12 +0000

    seg_vertex4 work without lines_approximation

--- lib/segmentation.h
+++ lib/segmentation.h
@@ -42,6 +42,9 @@ void seg_remove_contour(uint32 *reg, uint32 w, uint32 h);
 void seg_vertex_draw1(uint8 *img, Vertex **vp, uint32 vxc, uint32 w, uint32 h, uint32 k);
 void seg_vertex_draw2(uint8 *img, Vertex **vp, uint32 vxc, uint32 w, uint32 h, uint32 w1, uint32 h1);
 uint32 seg_vertex_draw3(uint8 *img, Vertex **vp, uint32 vxc, uint32 w, uint32 h, uint32 w1, uint32 h1);
+uint32  seg_vertex_draw4(uint8 *img, Vertex *vx, uint32 vxc, uint32 w, uint32 h, uint32 w1, uint32 h1);
+
+
 uint32  seg_get_or_fill_color(uint8 *img, uint8 *con, uint8 *col, uint32 *buff, Vertex **vp, uint8 *dir, uint32 rgc, uint32 w, uint32 h, uint32 w1, uint32 h1, uint32 get);
 uint32  seg_remove_loops(uint8 *img, Vertex **vp, Vertex **vp1, Vertex **vp2, Line *ln, uint8 *dir, uint32 vxc, uint32 w, uint32 h);
 uint32 seg_remove_inline(Vertex **vp, uint32 vxc, uint32 w, uint32 h);
